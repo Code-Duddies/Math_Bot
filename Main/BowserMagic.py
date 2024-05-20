6e671069c86714ed8f2fe9c64a7a43c6c413af5c
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-05-20 01:54:58 -0400

    Llama

--- Main/BowserMagic.py
+++
@@ -1,7 +0,0 @@
-from PageTools import PageTools
-from StorageClass import DataStorage
-from LoggerCalls import LoggerCalls
-
-lc = LoggerCalls()
-PT = PageTools(lc)
-PT.driver.get(DataStorage.string_dict['lama3_url'])
--- Main/StorageClass.py
+++ Main/StorageClass.py
@@ -5,5 +5,6 @@ from dataclasses import dataclass
 class DataStorage:
 	string_dict ={
 		'lama3_url' : 'https://llama.meta.com/llama3/',
-		'exerience_lama_button' : 'xt0psk2',
+		'exerience_lama_button' : '/html/body/div[1]/div/div/div/div[1]/div[1]/main/div/div[1]/div[2]/div/div/div/div/div/div[3]/div[2]/a',
+		'text_area' : '//*[@id=":r5:"]',
 	}
